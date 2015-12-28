a8267339e8c8828a247c7e909b12ce638ed8f4d9
adammartinez271828 <adam.paul.martinez@gmail.com>
2015-12-28 17:39:14 -0500

    Split mutation step into separate function.

--- evolution_algorithms.py
+++ evolution_algorithms.py
@@ -10,6 +10,29 @@ from scipy.stats import binom
 log = logging.getLogger(__name__)
 
 
+def truncation_with_mutation(population, fit_func, mutation_rate, mutation_func):
+    """Breed the next generation using a truncation selection algorithm with mutation.  The most fit
+    half of the population will survive to breeding age.  Each survivor will choose a spouse that is
+    not themselves at random and produce two children.  Each child will have mutation_rate chance
+    to mutate.
+
+    Args:
+        population: a list of Evolvers
+        fit_func: the fitness function used to determine survivors from generation to generation
+        mutation_rate: chance for each reproduction event to trigger a mutation event
+        mutation_func: the function responsible for controlling mutation.  Takes one argument of:
+            a gene, a chromosome, or a genotype, depending on the context of the mutation.
+
+    Returns:
+        the next generation of Evolvers
+    """
+    new_population = truncation_selection(population, fit_func)
+
+    mutate(new_population, mutation_rate, mutation_func)
+
+    return new_population
+
+
 def truncation_selection(population, fit_func):
     """Breed the next generation using a truncation selection algorithm.  The most fit half of the
     population will survive to breeding age.  Each survivor will choose a spouse that is not
@@ -35,45 +58,35 @@ def truncation_selection(population, fit_func):
     return new_population
 
 
-def truncation_with_mutation(population, fit_func, mutation_rate, mutation_func):
-    """Breed the next generation using a truncation selection algorithm with mutation.  The most fit
-    half of the population will survive to breeding age.  Each survivor will choose a spouse that is
-    not themselves at random and produce two children.  Each child will have mutation_rate chance
-    to mutate.
+def mutate(population, mutation_rate, mutation_func):
+    """
+
+    Given a population of n creatures each with a p chance of mutation, the number of
+    mutations in a given population obeys a binomial distribution.  i.e. given a population
+    of 100 creatures each with a 1% chance of mutation, you would expect to see ~1 mutation
+    per generation, but there is a ~36.6% chance 0 will mutate, a ~37% chance 1  will mutate, an
+    ~18.5% chance 2 will mutate, etc.
 
     Args:
         population: a list of Evolvers
-        fit_func: the fitness function used to determine survivors from generation to generation
-        mutation_rate: chance for each reproduction event to trigger a mutation event
-        mutation_func: the function responsible for controlling mutation.  Takes one argument of:
-            a gene, a chromosome, or a genotype, depending on the context of the mutation.
-
-    Returns:
-        the next generation of Evolvers
+        mutation_rate: chance for each Evolver to mutate
+        mutation_func: function that is applied to the Evolver's genome upon mutation
     """
-    new_population = truncation_selection(population, fit_func)
-
-    # Given a population of n creatures each with a p chance of mutation, the number of
-    # mutations in a given population obeys a binomial distribution.  i.e. given a population
-    # of 100 creatures each with a 1% chance of mutation, you would expect to see 1 mutation
-    # per generation on average, but there is a ~36.6% chance 0 will mutate, a ~37% chance 1
-    # will mutate, an ~18.5% chance 2 will mutate, etc.
-    num_mutations = binom.rvs(n=len(new_population), p=mutation_rate)
+    num_mutations = binom.rvs(n=len(population), p=mutation_rate)
     if num_mutations > 0:
-        # log.debug('Generating %d mutations in %d creatures.', num_mutations, len(new_population))
-        mutators = sample(new_population, num_mutations)
+        # log.debug('Generating %d mutations in %d creatures.', num_mutations, len(population))
+        mutators = sample(population, num_mutations)
         for mutator in mutators:
             mutator.mutate(mutation_func)
 
-    return new_population
+    return population
 
 
 def deranged(iterable):
-    """Generates a mathematical derangement of iterable.  i.e. take a sequence of items and return
-    a sequence of those same items in random order such that the nth item of the original sequence
-    and the nth item of the deranged sequence are never the same item.
+    """Generates a mathematical derangement of iterable.
 
-    This is actually implemented by just repeatedly calling random.shuffle on a copy of the iterable
+    A derangement is a permutation of a set such that no item remains in its original place.  This
+    is actually implemented by just repeatedly calling random.shuffle on a copy of the iterable
     until a derangement is found.  On average, random.shuffle will be called e (2.718...) times.
 
     Args:
